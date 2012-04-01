6e2781a938db039db6f9ce791f23bcdb0ddadfbb
JF Dionne <jeanfrancoisbd@gmail.com>
2012-04-01 19:31:57 -0400

    joink 1st commit

---
+++ joink/joink.cpp
@@ -0,0 +1,83 @@
+#define _CRT_SECURE_NO_DEPRECATE
+
+#include <stdio.h>
+#include <iostream>
+#include <memory.h>
+#include <assert.h>
+#include <algorithm>
+#include <functional>
+#include <vector>
+#include <string>
+#include <map>
+#include <set>
+#include <deque>
+#include <math.h>
+
+#define fo(a,b,c) for( a = ( b ); a < ( c );  a++ )
+#define fr(a,b) fo( a, 0, ( b ) )
+#define fi(a) fr( i, ( a ) )
+#define fj(a) fr( j, ( a ) )
+#define fk(a) fr( k, ( a ) )
+#define mp make_pair
+#define pb push_back
+#define all(v) (v).begin( ), (v).end( )
+#define _(a,b) memset( a, b, sizeof( a ) )
+
+using namespace std;
+int ni() {
+	int a;
+	scanf("%d", &a);
+	return a;
+}
+double nf() {
+	double a;
+	scanf("%lf", &a);
+	return a;
+}
+char sbuf[100005];
+string ns() {
+	scanf("%s", sbuf);
+	return sbuf;
+}
+long long nll() {
+	long long a;
+	scanf("%lld", &a);
+	return a;
+}
+
+typedef long long ll;
+typedef vector<ll> vi;
+typedef vector<string> vs;
+typedef set<int> si;
+typedef set<char> sc;
+typedef pair<int, int> pii;
+typedef map<string, int> msi;
+
+
+int solve(int n, int k) {
+   
+	char board[n][n];
+	int i,j;
+
+	for ( j = n-1 ; j >=0 ; j--)
+		fo(i,0,n){
+			cin >> board[j][i];
+			cout << board[j][i];
+		}
+	
+}
+
+
+int main() {
+
+    int i,t,n,k;
+	cin >> t;
+	
+    fo(i,0,t) {
+		cin >> n >> k;
+       cout << "Case #" << i+1 << ": " <<  solve(n,k) << endl;
+    }
+
+	return 0;
+}
+
