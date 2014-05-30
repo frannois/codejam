77db5f157c8f7a9e65c915a9e7d3869902b0dc6a
JF Dionne <jfdionne@immersion.com>
2014-05-30 18:44:57 -0400

    main not working

--- 2014/round 1A/promblemA/main.cpp
+++ 2014/round 1A/promblemA/main.cpp
@@ -1,25 +1,56 @@
-// my first program in C++
+
 #include <iostream>
 #include <string>
 
-string solve() {
+using namespace std;
+int n, l;
 
-  int i, l, n, t;
+int check(bool outlets[], bool  inputs[],int  index) {
+
+  // first make the switch
+  int i,j;
+  bool table[n];
+  index--;
+  if ( index > 0) {}
+  for (i = 0; i < n; i++ ){
+    outlets[i*l+index] = !outlets[i*l+index];
+  }
+
+
+
+}
+
+void solve() {
+
+  int i, tmp;
+  char t;
 
   cin >> n >> l;
 
-  int outlets[n][l];
-  int inputs[n][l];
+  bool outlets[n*l];
+  bool inputs[n*l];
 
+  // read the data
   for (i = 0 ; i < n*l ; i++) {
-    cin >>t;
-    outlets[i%n][i%l] = t;
-  }
-  for (i = 0 ; i < n*l ; i++) {
-    cin >>t;
-    inputs[i%n][i%l] = t;
+    std::cin >> t;
+    outlets[i] = (t == 1 ? true : false);
   }
 
+  for (i = 0 ; i < n*l ; i++) {
+    std::cin >>t;
+    inputs[i] = (t == 1 ? true : false);
+  }
+
+  // brute force
+  for (i = 0 ; i <= l ; i++) {
+    tmp = check(outlets,inputs,i);
+    if (tmp >= 0) {
+      std::cout << tmp << endl;
+      return;
+    }
+  }
+
+  std::cout << "NOT POSSIBLE" << endl;
 
 }
 
@@ -29,6 +60,7 @@ int main()
   int i, n;
   cin >> n;
   for (i = 1 ; i <= n ; i++) {
-    cout << "Case #" << i << ":" << solve();
+    std::cout << "Case #" << i << ":";
+    solve();
   }
 }
