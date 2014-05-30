085e857ea25715bde7a28aabf0de477ea279ff07
JF Dionne <jfdionne@Robot.local>
2014-05-30 09:03:13 -0400

    first commit four round 1 B

--- 2014/round 1A/promblemA/main.cpp
+++ 2014/round 1A/promblemA/main.cpp
@@ -1,34 +1,44 @@
-// my first program in C++
+
 #include <iostream>
 #include <string>
 
+using namespace std;
+
+int n, l;
+vector<string> outlets, inputs;
+
+
+
 string solve() {
+    
+    int i;
+    string t;
+    
+    cin >> n >> l;
+    
+    for (i = 0 ; i < n ; i++) {
+        cin >> outlets[i];
+    }
+    for (i = 0 ; i < n ; i++) {
+        cin >> inputs[i];
+    }
 
-  int i, l, n, t;
-
-  cin >> n >> l;
-
-  int outlets[n][l];
-  int inputs[n][l];
-
-  for (i = 0 ; i < n*l ; i++) {
-    cin >>t;
-    outlets[i%n][i%l] = t;
-  }
-  for (i = 0 ; i < n*l ; i++) {
-    cin >>t;
-    inputs[i%n][i%l] = t;
-  }
-
-
+    sort(outlets.begin(), outlets.end());
+    
+    for (i = 0; i < n ; i++) {
+        vector<string> d = outlets;
+        
+    }
+    
+    
 }
 
 
 int main()
 {
-  int i, n;
-  cin >> n;
-  for (i = 1 ; i <= n ; i++) {
-    cout << "Case #" << i << ":" << solve();
-  }
+    int i, j;
+    cin >> j;
+    for (i = 1 ; i <= j ; i++) {
+        cout << "Case #" << i << ":" << solve();
+    }
 }
